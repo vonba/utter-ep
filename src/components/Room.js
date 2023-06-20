db9727e8d0cb269b3fbd2f9a4f3f6b49ef28bd82
Tom Kearney <tom.f.kearney@gmail.com>
2023-06-19 18:03:36 -0600

    new room

--- src/components/Room.js
+++ src/components/Room.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import VideoBackground from "./VideoBackground";
 import RoomDesire from "./RoomDesire";
 import RoomItShines from "./RoomItShines";
+import RoomSoundAndTouch from "./RoomSoundAndTouch";
 
 const RoomStyles = styled.div`
   position: fixed;
@@ -26,31 +27,37 @@ const RoomStyles = styled.div`
   }
 `;
 
-const videoNames ={
-  'desire': 'desire.mov',
-  'it-shines': 'it-shines.mov',
-  'sound-and-touch': 'sound-and-touch.mov',
-}
+const videoNames = {
+  desire: "desire.mov",
+  "it-shines": "it-shines.mov",
+  "sound-and-touch": "sound-and-touch.mov",
+};
 
-export default function Room({roomName}) {
+export default function Room({ roomName }) {
   const videoName = videoNames[roomName];
 
   const nextRoom = () => {
     // TODO: move to the next room
-  }
+  };
 
-  return <RoomStyles>
-    <VideoBackground videoName={videoName} handleVideoEnd={nextRoom} />
-    {roomName === 'desire' && (
-      <div className="roomWrapper">
-        <RoomDesire className="room" />
-      </div>
-    )}
-    {roomName === 'it-shines' && (
-      <div className="roomWrapper">
-        <RoomItShines className="room" />
-      </div>
-    )}
-  </RoomStyles>
+  return (
+    <RoomStyles>
+      <VideoBackground videoName={videoName} handleVideoEnd={nextRoom} />
+      {roomName === "desire" && (
+        <div className="roomWrapper">
+          <RoomDesire className="room" />
+        </div>
+      )}
+      {roomName === "it-shines" && (
+        <div className="roomWrapper">
+          <RoomItShines className="room" />
+        </div>
+      )}
+      {roomName === "sound-and-touch" && (
+        <div className="roomWrapper">
+          <RoomSoundAndTouch className="room" />
+        </div>
+      )}
+    </RoomStyles>
+  );
 }
-
